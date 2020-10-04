6c6c0ce0196df1e49090a56f7a1ca59eb7fa016d
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2020-10-04 17:57:35 +0530

    add 04_10.py

---
+++ 04_10.py
@@ -0,0 +1,16 @@
+# Remove Covered Intervals
+
+# Sort the start in ascending order.
+# In-case the start is same, then sort according to the end in descending order.
+
+class Solution:
+	def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+		intervals = sorted(intervals, key = lambda x: (x[0], -x[1]))
+
+		count = 0
+		ending = 0
+		for start, end in intervals:
+			if end > ending:
+				count += 1
+				ending = end
+		return count
