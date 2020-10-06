168abec7cf296fa8d0ac1ca52877f67e32cfb0ed
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2020-10-06 18:21:54 +0530

    add 06_10.py

---
+++ 06_10.py
@@ -0,0 +1,12 @@
+# Insert into a Binary Search Tree
+
+class Solution:
+	def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+		if not root:
+			root = TreeNode(val = val)
+		elif root.val > val:
+			root.left = self.insertIntoBST(root.left, val)
+		else:
+			root.right = self.insertIntoBST(root.right, val)
+
+		return root
