0529c70a80d91f14e6214b1cd72868968a70937b
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2020-10-06 14:02:35 +0530

    add 06_10.py

--- 06_10.py
+++ 06_10.py
@@ -10,10 +10,8 @@ class Solution:
 	def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
 		if not root:
 			root = TreeNode(val = val)
-
 		elif root.val > val:
 			root.left = self.insertIntoBST(root.left, val)
-
 		else:
 			root.right = self.insertIntoBST(root.right, val)
 
