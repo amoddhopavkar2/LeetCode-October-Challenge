f6fe04f5665f55c566da8a0c3ea2f806079bd909
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2020-10-07 13:05:28 +0530

    add 07_10.py

---
+++ 07_10.py
@@ -0,0 +1,28 @@
+# Rotate Linked List
+
+class Solution:
+	def rotateRight(self, head: ListNode, k: int) -> ListNode:
+		if not head or not k:
+			return head
+
+		n, tail, current = 0, head, head
+		while current and current.next:
+			current = current.next
+			n += 1
+		tail = current
+		n += 1
+
+		k = k % n
+		if not k:
+			return head
+
+		n = n - k - 1
+		current = head
+		for _ in range(n):
+			current = current.next
+
+		new_head = current.next
+		current.next = None
+		tail.next = head
+
+		return new_head
