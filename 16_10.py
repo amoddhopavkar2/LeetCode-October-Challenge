4c2edd7c464c242422b3ea06e15def91aca2d10b
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2020-10-16 14:00:15 +0530

    rm 16_10.py

--- 16_10.py
+++
@@ -1,8 +0,0 @@
-# Search in 2D Matrix
-
-class Solution:
-	def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-		for i in range(len(matrix)):
-			if len(matrix[i]) > 0 and target >= matrix[i][0] and target <= matrix[i][-1] and target in matrix[i]:
-				return True
-		return False
