16574f36c72df50aecede90fd0e4850318ed6c73
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2020-10-17 09:45:01 +0530

    add 16_10.py

--- 16_10.py
+++ 16_10.py
@@ -1,7 +1,7 @@
-# Search 2-D Matrix
+# Search a 2-D Matrix
 
 class Solution:
-    def searchMatrix(self, matrix: [List[List[int]]], target: int) -> bool:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         for i in range(len(matrix)):
             if len(matrix[i]) > 0 and target >= matrix[i][0] and target <= matrix[i][-1] and target in matrix[i]:
                 return True
