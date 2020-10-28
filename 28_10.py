1baa0fbdd3cb746768c8b2b9dd3b66c4c435ee97
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2020-10-28 13:39:29 +0530

    add 28_10.py

---
+++ 28_10.py
@@ -0,0 +1,18 @@
+# Summary Ranges
+
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        result = []
+        
+        for i in nums:
+            if i-1 not in nums:
+                y = i + 1
+                while y in nums:
+                    y += 1
+                if i == y - 1:
+                    result.append(str(i))
+                    
+                else:
+                    result.append(str(i)+"->"+str(y-1))
+        
+        return result
